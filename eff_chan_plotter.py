f15e1700f11c5b5982d85e399ab28b070c27eb57
salvis2 <sebialvis@gmail.com>
2018-02-09 10:27:54 -0800

    Convert the script into a module
    The creation of full_eff_plotter.py necessitated the conversion of this script into a module.

--- eff_chan_plotter.py
+++ eff_chan_plotter.py
@@ -1,41 +1,43 @@
-import os, sys
+# Module to take the concatenated outputs of DCRcomparison.DCRComp
+# and plot efficiencies for each channel over all the calibration run ranges
+# requires the existence of eff_sheet.txt
+
+import os, sys, argparse
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-import pandas as pd
 
-# Read in the efficiency txt file
-efficiencies = [[],[],[],[]]
-with open("eff_sheet.txt") as efficiency_file:
-    for line in efficiency_file:
+def eff_plot(dest_folder,ds):
+
+  # Read in the efficiency txt file
+  efficiencies = [[],[],[],[]]
+  # ideally, dest_folder is /global/projecta/projectdirs/majorana/users/salvis/analysis/DCR/quality/plots/
+  txtfile = dest_folder + "eff_sheet.txt"
+  with open(txtfile) as efficiency_file:
+      for line in efficiency_file:
         data = line.split()
         efficiencies[0].append(int(data[0]))
         efficiencies[1].append(int(data[1]))
         efficiencies[2].append(int(data[2]))
         efficiencies[3].append(float(data[3]))
 
-# print(len(efficiencies)) # Gives 4
-# print(len(efficiencies[0])) # Gives 1008
+  # efficiencies[0] = start runs
+  # efficiencies[1] = end runs
+  # efficiencies[2] = channels
+  # efficiencies[3] = dcr90 efficiencies
 
-# efficiencies[0] = start runs
-# efficiencies[1] = end runs
-# efficiencies[2] = channels
-# efficiencies[3] = dcr90 efficiencies
+  # Pdf name check
+  pdf_name = dest_folder + 'Efficiencies_Over_Time_'
+  if (ds == 'DS6'): pdf_name += 'DS6.pdf'
+  elif (ds == 'DS5cM1'): pdf_name += 'DS5cM1.pdf'
+  elif (ds == 'DS5cM2'): pdf_name += 'DS5cM2.pdf'
+  else: raise ValueError('Incorrect DataSet')
 
-# Save to big pdf
-with PdfPages('/global/projecta/projectdirs/majorana/users/salvis/analysis/DCR/quality/plots/Efficiencies_Over_Time.pdf') as pdf:
-    # Want to plot each channel's efficiencies as a function of run
-    # Sort the array by channel
-    #indices = range(len(efficiencies[0]))
-    #indices.sort(key = efficiencies[2].__getitem__)
-    #for i, sublist in enumerate(efficiencies):
-        #efficiencies[i] = [sublist[j] for j in indices]
-
-    # Get something I can actually iterate over usefully
-    channels = set(efficiencies[2])
-    print(channels)
-    channels = sorted(channels)
-    print(channels)
-    for chan in channels:
+  # Save to big pdf
+  with PdfPages(pdf_name) as pdf:
+      # Sort the array by channel
+      channels = set(efficiencies[2])
+      channels = sorted(channels)
+      for chan in channels:
         # Find the range in array that corresponds to this channel
         chan_effs = [[],[],[],[]]
         for i in range(0,len(efficiencies[0])):
@@ -48,6 +50,8 @@ with PdfPages('/global/projecta/projectdirs/majorana/users/salvis/analysis/DCR/q
         plt.title('Channel %d DCR 90 Efficiency Over Time' % chan)
         plt.xlabel('Starting Run')
         plt.ylabel('DCR 90 Efficiency')
-        #plt.show()
         pdf.savefig()
         plt.close()
+
+if __name__ == "__main__":
+  eff_plot(str(sys.argv[1]),int(sys.argv[2]))
